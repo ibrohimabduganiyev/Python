f600dc4a9fd4cb64c8a4462fa8097a2a6a3a4148
deku <89076165+ibrohimabduganiyev@users.noreply.github.com>
2025-08-09 00:41:39 +0500

    Create script.py

---
+++ lesson-1/script.py
@@ -0,0 +1,31 @@
+a = 5
+
+p = 4 * a
+s= a ** 2
+
+print(f"perimetri {p}, yuzasi {s}")
+
+###
+
+d = 20
+l = 3.14 * d
+print(f'doira uzinligi {l}')
+
+###
+
+f = 5
+j = 9
+mean = (f+j)//2
+print(mean)
+
+###
+
+c = 3
+e = 4
+
+sum = c + e
+product = c * e
+c_square = c ** 2
+e_square = e ** 2
+
+print(f"yigindisi {sum}, ko'paytmasi {product}, c kvadrati {c_square}, e kvadarti {e_square}")
